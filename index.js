89c89b638f0f2e1d8a71ea7091224967d384a131
masudcodex <forester.masud@gmail.com>
2022-11-25 02:34:25 +0600

    users database created and connected to the client side sign in

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -11,6 +12,42 @@ app.use(express.json());
 app.get('/', (req, res)=> {
     res.send("Furnishbay Server is running");
 })
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f75ntdx.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+        const userCollection = client.db('furnishbay').collection('users');
+
+        app.post('/users', async(req, res)=> {
+            const user = req.body;
+            console.log(user);
+            const query = {email: user.email}
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+    }finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 
 app.listen(port, (req, res)=> {
     console.log(`Furnishbay Server is running at port ${port}`);
