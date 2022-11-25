b049aeba4b43e6a2dd30a94b10d6a30715dbca5e
masudcodex <forester.masud@gmail.com>
2022-11-25 12:49:49 +0600

    jsonwebtoken and verify token setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -13,21 +14,48 @@ app.get('/', (req, res)=> {
     res.send("Furnishbay Server is running");
 })
 
-
+//verifyJWT setup
+function verifyJwt(req, res, next){
+            
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).send('Unauthorized access')
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.JWT_TOKEN, function(err, decoded){
+        if (err) {
+            return res.status(403).send({message: 'Access forbidden'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f75ntdx.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 async function run(){
     try{
 
         const userCollection = client.db('furnishbay').collection('users');
 
+        //Get JWT
+        app.get('/jwt', async(req, res)=>{
+            const email = req.query.email;
+            const query = {email: email};
+            const user = await userCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.JWT_TOKEN, {expiresIn: '2d'})
+                return res.send({accessToken: token})
+            }
+            res.status(403).send({accessToken: ''})
+        })
+
+
         app.post('/users', async(req, res)=> {
             const user = req.body;
-            console.log(user);
             const query = {email: user.email}
             const existingUser = await userCollection.findOne(query);
             if (existingUser) {
